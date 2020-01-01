6e77ef346f4635b8935fe074aecf1fd4535cada6
HarJIT <harjit@harjit.moe>
2020-01-01 15:11:43 +0000

    Make SCSU use only one docsmode, and fix a bug I happened to notice.
    (UDX, unlike UD, hadn't been set to actually switch back to 1-byte mode.)

--- scsufilter.py
+++ scsufilter.py
@@ -28,154 +28,156 @@ def decode_scsu(stream, state):
     for token in stream:
         if (token[0] == "DOCS"):
             if token == scsudocs:
-                state.bytewidth = 1
-                state.docsmode = "scsu-1byte"
+                state.docsmode = "scsu"
+                state.scsumode = 1
                 state.cur_dynwindows = [0x1, 0xF9, 0x8, 0xC, 0x12, 0xFD, 0xFE, 0xA6]
                 state.cur_windex = 0
+                # Note: is 1 even in "Unicode" mode, because mixed 1-byte controls and 2-byte words
+                state.bytewidth = 1
             else:
                 yield token
-        elif state.docsmode == "scsu-1byte" and token[0] == "WORD":
+        elif state.docsmode == "scsu" and token[0] == "WORD":
             assert (token[1] < 0x100), token
-            if pending is None:
-                if token[1] in (0x0, 0x9, 0xA, 0xD):
-                    yield ("UCS", token[1], "SCSU", "SCSU-ASCII")
-                elif token[1] < 0x9: # SQn
-                    pending = ("SQ", token[1] - 1)
-                elif token[1] == 0xB: # SDX
-                    pending = ("SDX", None)
-                elif token[1] == 0xC: # reserved
-                    yield ("ERROR", "SCSUONEBYTEUNDEFCTRL", token[1])
-                elif token[1] == 0xE: # SQU
-                    pending = ("SQU", None)
-                elif token[1] == 0xF: # SCU
-                    yield ("SCSUSHIFT", -1)
-                    # Can't use 2, since there are 1-byte controls mixed in.
-                    state.bytewidth = 1
-                    state.docsmode = "scsu-2byte"
-                elif token[1] < 0x18: # SCn
-                    yield ("SCSUSHIFT", token[1] - 16)
-                    state.cur_windex = token[1] - 16
-                elif token[1] < 0x20: # SDn
-                    pending = ("SD", token[1] - 24)
-                elif 0x20 <= token[1] < 0x80:
-                    yield ("UCS", token[1], "SCSU", "SCSU-ASCII")
-                else:
-                    window = state.cur_dynwindows[state.cur_windex]
+            if state.scsumode == 1:
+                if pending is None:
+                    if token[1] in (0x0, 0x9, 0xA, 0xD):
+                        yield ("UCS", token[1], "SCSU", "SCSU-ASCII")
+                    elif token[1] < 0x9: # SQn
+                        pending = ("SQ", token[1] - 1)
+                    elif token[1] == 0xB: # SDX
+                        pending = ("SDX", None)
+                    elif token[1] == 0xC: # reserved
+                        yield ("ERROR", "SCSUONEBYTEUNDEFCTRL", token[1])
+                    elif token[1] == 0xE: # SQU
+                        pending = ("SQU", None)
+                    elif token[1] == 0xF: # SCU
+                        yield ("SCSUSHIFT", -1)
+                        state.scsumode = 2
+                    elif token[1] < 0x18: # SCn
+                        yield ("SCSUSHIFT", token[1] - 16)
+                        state.cur_windex = token[1] - 16
+                    elif token[1] < 0x20: # SDn
+                        pending = ("SD", token[1] - 24)
+                    elif 0x20 <= token[1] < 0x80:
+                        yield ("UCS", token[1], "SCSU", "SCSU-ASCII")
+                    else:
+                        window = state.cur_dynwindows[state.cur_windex]
+                        offset = _get_offset(window)
+                        if offset is None:
+                            yield ("ERROR", "SCSUINVALIDWINDOW", (window, token[1] - 0x80))
+                        else:
+                            yield ("UCS", token[1] - 0x80 + offset, "SCSU", "SCSU-LDW")
+                elif pending[0] == "SQ":
+                    window = state.cur_dynwindows[pending[1]]
                     offset = _get_offset(window)
-                    if offset is None:
-                        yield ("ERROR", "SCSUINVALIDWINDOW", (window, token[1] - 0x80))
+                    if token[1] >= 0x80 and offset is not None:
+                        if state.cur_windex == pending[1]:
+                            yield ("ERROR", "SCSUREDUNDANTSQDW", token[1])
+                            yield ("UCS", token[1] - 0x80 + offset, "SCSU", "SCSU-SDW-REDUNDANT")
+                        else:
+                            yield ("UCS", token[1] - 0x80 + offset, "SCSU", "SCSU-SDW")
+                    elif token[1] < 0x80:
+                        offset = _staticoffset[pending[1]]
+                        if (offset == 0) and ((token[1] > 0x1F) or (token[1] in (0x0, 0x9, 0xA, 0xD))):
+                            yield ("ERROR", "SCSUREDUNDANTSQZERO", token[1])
+                            yield ("UCS", token[1] + offset, "SCSU", "SCSU-SW-REDUNDANT")
+                        else:
+                            yield ("UCS", token[1] + offset, "SCSU", "SCSU-SW")
                     else:
-                        yield ("UCS", token[1] - 0x80 + offset, "SCSU", "SCSU-LDW")
-            elif pending[0] == "SQ":
-                window = state.cur_dynwindows[pending[1]]
-                offset = _get_offset(window)
-                if token[1] >= 0x80 and offset is not None:
-                    if state.cur_windex == pending[1]:
-                        yield ("ERROR", "SCSUREDUNDANTSQDW", token[1])
-                        yield ("UCS", token[1] - 0x80 + offset, "SCSU", "SCSU-SDW-REDUNDANT")
+                        yield ("ERROR", "SCSUINVALIDWINDOW", (pending[1], token[1] - 0x80))
+                    pending = None
+                elif pending[0] == "SQU":
+                    if pending[1] is None:
+                        pending = (pending[0], token[1])
                     else:
-                        yield ("UCS", token[1] - 0x80 + offset, "SCSU", "SCSU-SDW")
-                elif token[1] < 0x80:
-                    offset = _staticoffset[pending[1]]
-                    if (offset == 0) and ((token[1] > 0x1F) or (token[1] in (0x0, 0x9, 0xA, 0xD))):
-                        yield ("ERROR", "SCSUREDUNDANTSQZERO", token[1])
-                        yield ("UCS", token[1] + offset, "SCSU", "SCSU-SW-REDUNDANT")
+                        word = (pending[1] << 8) | token[1]
+                        if word < 0xD800 or word >= 0xE000:
+                            yield ("UCS", word, "SCSU", "SCSU-SUCS")
+                        else:
+                            # No surrogates in SQU.
+                            yield ("ERROR", "SCSUSQUSURROGATE", word)
+                            yield ("UCS", word, "SCSU", "WTF-SCSU-SUCS")
+                        pending = None
+                elif pending[0] == "SD":
+                    yield ("SCSUDESIG", pending[1], token[1])
+                    state.cur_dynwindows[pending[1]] = token[1]
+                    state.cur_windex = pending[1]
+                    pending = None
+                elif pending[0] == "SDX":
+                    if pending[1] is None:
+                        pending = (pending[0], token[1])
                     else:
-                        yield ("UCS", token[1] + offset, "SCSU", "SCSU-SW")
+                        windex = pending[1] >> 5
+                        window = ((pending[1] & 0x1F) << 8) | token[1]
+                        # One's complement it to distinguish it from the positive ones, even zero.
+                        yield ("SCSUDESIG", windex, ~window)
+                        state.cur_dynwindows[windex] = ~window
+                        state.cur_windex = windex
+                        pending = None
                 else:
-                    yield ("ERROR", "SCSUINVALIDWINDOW", (pending[1], token[1] - 0x80))
-                pending = None
-            elif pending[0] == "SQU":
-                if pending[1] is None:
-                    pending = (pending[0], token[1])
-                else:
+                    raise AssertionError(pending)
+            else:
+                assert state.scsumode == 2, state.scsumode
+                if pending is None:
+                    if token[1] <= 0xDF or token[1] >= 0xF3:
+                        # i.e. start of a directly encoded UTF-16 representation
+                        pending = ("UTF-16", token[1])
+                    elif 0xE0 <= token[1] < 0xE8: # UCn
+                        yield ("SCSUSHIFT", token[1] - 0xE0)
+                        state.cur_windex = token[1] - 0xE0
+                        state.scsumode = 1
+                    elif 0xE8 <= token[1] < 0xF0: # UDn
+                        pending = ("UD", token[1] - 0xE8)
+                    elif token[1] == 0xF0: # UQU
+                        pending = ("UQU", None)
+                    elif token[1] == 0xF1: # UDX
+                        pending = ("UDX", None)
+                    else:
+                        yield ("ERROR", "SCSUTWOBYTEUNDEFCTRL", token[1])
+                elif pending[0] == "UTF-16":
                     word = (pending[1] << 8) | token[1]
                     if word < 0xD800 or word >= 0xE000:
-                        yield ("UCS", word, "SCSU", "SCSU-SUCS")
+                        yield ("UCS", word, "SCSU", "SCSU-DUCS")
                     else:
-                        # No surrogates in SQU.
-                        yield ("ERROR", "SCSUSQUSURROGATE", word)
-                        yield ("UCS", word, "SCSU", "WTF-SCSU-SUCS")
+                        # Pass through to UTF-16 filter.
+                        yield ("PAIR", word, "SCSU", "SCSU-DUCS")
                     pending = None
-            elif pending[0] == "SD":
-                yield ("SCSUDESIG", pending[1], token[1])
-                state.cur_dynwindows[pending[1]] = token[1]
-                state.cur_windex = pending[1]
-                pending = None
-            elif pending[0] == "SDX":
-                if pending[1] is None:
-                    pending = (pending[0], token[1])
-                else:
-                    windex = pending[1] >> 5
-                    window = ((pending[1] & 0x1F) << 8) | token[1]
-                    # One's complement it to distinguish it from the positive ones, even zero.
-                    yield ("SCSUDESIG", windex, ~window)
-                    state.cur_dynwindows[windex] = ~window
-                    state.cur_windex = windex
-                    pending = None
-            else:
-                raise AssertionError(pending)
-        elif state.docsmode == "scsu-2byte" and token[0] == "WORD":
-            assert (token[1] < 0x100), token
-            if pending is None:
-                if token[1] <= 0xDF or token[1] >= 0xF3:
-                    # i.e. start of a directly encoded UTF-16 representation
-                    pending = ("UTF-16", token[1])
-                elif 0xE0 <= token[1] < 0xE8: # UCn
-                    yield ("SCSUSHIFT", token[1] - 0xE0)
-                    state.cur_windex = token[1] - 0xE0
-                    state.bytewidth = 1
-                    state.docsmode = "scsu-1byte"
-                elif 0xE8 <= token[1] < 0xF0: # UDn
-                    pending = ("UD", token[1] - 0xE8)
-                elif token[1] == 0xF0: # UQU
-                    pending = ("UQU", None)
-                elif token[1] == 0xF1: # UDX
-                    pending = ("UDX", None)
-                else:
-                    yield ("ERROR", "SCSUTWOBYTEUNDEFCTRL", token[1])
-            elif pending[0] == "UTF-16":
-                word = (pending[1] << 8) | token[1]
-                if word < 0xD800 or word >= 0xE000:
-                    yield ("UCS", word, "SCSU", "SCSU-DUCS")
-                else:
-                    # Pass through to UTF-16 filter.
-                    yield ("PAIR", word, "SCSU", "SCSU-DUCS")
-                pending = None
-            elif pending[0] == "UQU":
-                if pending[1] is None:
-                    pending = (pending[0], token[1])
-                else:
-                    word = (pending[1] << 8) | token[1]
-                    if 0xD800 <= word < 0xE000:
-                        yield ("ERROR", "SCSUUQUSURROGATE", word)
-                        yield ("UCS", word, "SCSU", "WTF-SCSU-DUCS-UQU")
-                    elif 0xE000 <= word < 0xF300:
-                        yield ("UCS", word, "SCSU", "SCSU-DUCS-UQU")
+                elif pending[0] == "UQU":
+                    if pending[1] is None:
+                        pending = (pending[0], token[1])
                     else:
-                        yield ("ERROR", "SCSUREDUNDANTUQU", word)
-                        yield ("UCS", word, "SCSU", "SCSU-DUCS-UQU-REDUNDANT")
+                        word = (pending[1] << 8) | token[1]
+                        if 0xD800 <= word < 0xE000:
+                            yield ("ERROR", "SCSUUQUSURROGATE", word)
+                            yield ("UCS", word, "SCSU", "WTF-SCSU-DUCS-UQU")
+                        elif 0xE000 <= word < 0xF300:
+                            yield ("UCS", word, "SCSU", "SCSU-DUCS-UQU")
+                        else:
+                            yield ("ERROR", "SCSUREDUNDANTUQU", word)
+                            yield ("UCS", word, "SCSU", "SCSU-DUCS-UQU-REDUNDANT")
+                        pending = None
+                elif pending[0] == "UD":
+                    yield ("SCSUDESIG", pending[1], token[1])
+                    state.cur_dynwindows[pending[1]] = token[1]
+                    state.cur_windex = pending[1]
+                    state.scsumode = 1
                     pending = None
-            elif pending[0] == "UD":
-                yield ("SCSUDESIG", pending[1], token[1])
-                state.cur_dynwindows[pending[1]] = token[1]
-                state.cur_windex = pending[1]
-                state.bytewidth = 1
-                state.docsmode = "scsu-1byte"
-                pending = None
-            elif pending[0] == "UDX":
-                if pending[1] is None:
-                    pending = (pending[0], token[1])
+                elif pending[0] == "UDX":
+                    if pending[1] is None:
+                        pending = (pending[0], token[1])
+                    else:
+                        windex = pending[1] >> 5
+                        window = ((pending[1] & 0x1F) << 8) | token[1]
+                        # One's complement it to distinguish it from the positive ones, even zero.
+                        yield ("SCSUDESIG", windex, ~window)
+                        state.cur_dynwindows[windex] = ~window
+                        state.cur_windex = windex
+                        state.scsumode = 1
+                        pending = None
                 else:
-                    windex = pending[1] >> 5
-                    window = ((pending[1] & 0x1F) << 8) | token[1]
-                    # One's complement it to distinguish it from the positive ones, even zero.
-                    yield ("SCSUDESIG", windex, ~window)
-                    state.cur_dynwindows[windex] = ~window
-                    state.cur_windex = windex
-                    pending = None
-            else:
-                raise AssertionError(pending)
+                    raise AssertionError(pending)
+                #
+            #
         else:
             yield token
         #
