7e5c9252f885b73ab3decdf7371c2a8b165d7666
HarJIT <harjit@harjit.moe>
2020-09-22 09:44:16 +0100

    Deal with CJK / Tangut / Hangul in ecma35.data.names.namedata.

--- ecma35/data/names/namedata.py
+++ ecma35/data/names/namedata.py
@@ -7,12 +7,46 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 import os, json, xml.dom.minidom, re
-import unicodedata as ucd
 
 directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "namemaps")
 _no_default = object() # need a unique featureless object which isn't used anywhere else.
 _nonword = re.compile(r"\W+", re.U)
 _nonword2 = re.compile(r"[^\w-]+", re.U)
+
+def _is_cjkui(ucs):
+    # Note: excludes the Dirty Dozen
+    if len(ucs) > 1:
+        return False
+    code = ord(ucs)
+    if (0x4E00 <= code < 0xA000) or (0x3400 <= code < 0x4DC0) or (0x20000 <= code < 0x2F800) or (
+                                     0x30000 <= code < 0x3FFFE):
+        return True
+    return False
+
+def _is_cjkci(ucs):
+    # Note: includes the Dirty Dozen
+    if len(ucs) > 1:
+        return False
+    code = ord(ucs)
+    if (0xF900 <= code < 0xFB00) or (0x2F800 <= code < 0x2FFFE):
+        return True
+    return False
+
+def _is_tangut(ucs):
+    if len(ucs) > 1:
+        return False
+    code = ord(ucs)
+    if (0x17000 <= code < 0x18800) or (0x18D00 <= code < 0x18D90):
+        return True
+    return False
+
+def _is_pua(ucs):
+    if len(ucs) > 1:
+        return False
+    code = ord(ucs)
+    if (0xE000 <= code < 0xF900) or (0xF0000 <= code < 0xFFFFE) or (0x100000 <= code < 0x10FFFE):
+        return True
+    return False
 
 ucsnames = {}
 rucsnames = {}
@@ -29,13 +63,16 @@ with open(os.path.join(directory, "UCD/UnicodeData.txt"), "r") as f:
             ucsnames[_ucs] = _ucsname
             rucsnames[_ucsname] = _ucs
         if _decompos:
-            if _decompos[0] != "<": # i.e. if it exists and is of canonical type
+            if _decompos[0] != "<" and not _is_cjkci(_ucs):
                 _decompos = "".join(chr(int(_j, 16)) for _j in _decompos.split())
                 canonical_decomp[_ucs] = _decompos
-            else:
+            elif _decompos[0] == "<":
                 _dectype, _decompos = _decompos[1:].split("> ", 1)
                 _decompos = "".join(chr(int(_j, 16)) for _j in _decompos.split())
                 compat_decomp[_ucs] = (_dectype, _decompos)
+            else:
+                _decompos = "".join(chr(int(_j, 16)) for _j in _decompos.split())
+                compat_decomp[_ucs] = ("", _decompos)
         ucscats[_ucs] = _ucscat
 with open(os.path.join(directory, "UCD/NamedSequences.txt"), "r") as f:
     for line in f:
@@ -45,10 +82,66 @@ with open(os.path.join(directory, "UCD/NamedSequences.txt"), "r") as f:
         _ucs = "".join(chr(int(_i, 16)) for _i in _ucs.split())
         ucsnames[_ucs] = _ucsname
         rucsnames[_ucsname] = _ucs
+with open(os.path.join(directory, "UCD/NameAliases.txt"), "r") as f:
+    for line in f:
+        if not line.strip() or (line[0] == "#"):
+            continue
+        _ucs, _ucsname, status = line.split(";", 2)
+        _ucs = "".join(chr(int(_i, 16)) for _i in _ucs.split())
+        rucsnames[_ucsname] = _ucs
+        if status.strip() != "abbreviation":
+            # Generally prefer these ones.
+            ucsnames[_ucs] = _ucsname
+
+# Sort out the Korean syllables (which are not listed individually)
+# These romanisation tables match those used by the unicodedata module for syllables, see below.
+_koinit = ("G", "GG", "N", "D", "DD", "R", "M", "B", "BB", "S", "SS",
+           "", "J", "JJ", "C", "K", "T", "P", "H")
+_komed = ("A", "AE", "YA", "YAE", "EO", "E", "YEO", "YE", "O", "WA", 
+          "WAE", "OE", "YO", "U", "WEO", "WE", "WI", "YU", "EU", "YI", "I")
+_kofin = ("", "G", "GG", "GS", "N", "NJ", "NH", "D", "L", "LG", "LM", "LB", "LS", 
+          "LT", "LP", "LH", "M", "B", "BS", "S", "SS", "NG", "J", "C", "K", "T", "P", "H")
+for _initial in range(19):
+    for _medial in range(21):
+        for _final in range(28):
+            _syllcode = chr(_initial * (28 * 21) + _medial * 28 + _final + 0xAC00)
+            _initcode = chr(0x1100 + _initial)
+            _medialcode = chr(0x1161 + _medial)
+            _finalcode = chr(0x11A7 + _final) if _final else None
+            if _finalcode:
+                canonical_decomp[_syllcode] = _initcode + _medialcode + _finalcode
+            else:
+                canonical_decomp[_syllcode] = _initcode + _medialcode
+            _initname = ucsnames[_initcode].rsplit("CHOSEONG", 1)[1]
+            _medialname = ucsnames[_medialcode].rsplit("JUNGSEONG", 1)[1]
+            _finalname = ucsnames[_finalcode].rsplit("JONGSEONG", 1)[1] if _finalcode else ""
+            # Names used in JOHAB.TXT:
+            _name1 = "HANGUL SYLLABLE" + _initname + _medialname + _finalname.replace("-", "")
+            # Names used in KSC5601.TXT:
+            _name2 = _name1.replace(" ", "-").replace("-SYLLABLE-", " SYLLABLE ")
+            # Names used in OLD5601.TXT (though obviously the codepoints have changed):
+            _name3 = "HANGUL SYLLABLE" + _initname + _medialname + _finalname
+            # For the sake thereof, since it fits the pattern of the other "generated" names:
+            _name4 = "HANGUL SYLLABLE-{:04X}".format(ord(_syllcode))
+            # unicodedata module compatible; presumably normative(?), but not in NamesList.txt:
+            _name5 = "HANGUL SYLLABLE " + _koinit[_initial] + _komed[_medial] + _kofin[_final]
+            rucsnames[_name1] = _syllcode
+            rucsnames[_name2] = _syllcode
+            rucsnames[_name3] = _syllcode
+            rucsnames[_name4] = _syllcode
+            rucsnames[_name5] = _syllcode
+            ucsnames[_syllcode] = _name5
+            ucscats[_syllcode] = "Lo"
 
 def get_ucsname(ucs, default=_no_default):
     if ucs in ucsnames:
         return ucsnames[ucs]
+    elif _is_cjkui(ucs):
+        return "CJK UNIFIED IDEOGRAPH-{:04X}".format(ord(ucs))
+    elif _is_cjkci(ucs):
+        return "CJK COMPATIBILITY IDEOGRAPH-{:04X}".format(ord(ucs))
+    elif _is_tangut(ucs):
+        return "TANGUT IDEOGRAPH-{:04X}".format(ord(ucs))
     elif default is _no_default:
         raise KeyError("no known UCS name: {!r}".format(ucs))
     else:
@@ -57,6 +150,9 @@ def get_ucsname(ucs, default=_no_default):
 def lookup_ucsname(name, default=_no_default):
     if name in rucsnames:
         return rucsnames[name]
+    elif "IDEOGRAPH-" in name:
+        carenot, ucs = name.rsplit("IDEOGRAPH-", 1)
+        return chr(int(ucs, 16))
     elif default is _no_default:
         raise KeyError("unrecognised UCS name: {!r}".format(name))
     else:
@@ -65,6 +161,10 @@ def lookup_ucsname(name, default=_no_default):
 def get_ucscategory(ucs, default=_no_default):
     if ucs in ucscats:
         return ucscats[ucs]
+    elif _is_cjkui(ucs) or _is_cjkci(ucs) or  _is_tangut(ucs):
+        return "Lo"
+    elif _is_pua(ucs):
+        return "Co"
     elif default is _no_default:
         raise KeyError("no known UCS category: {!r}".format(ucs))
     else:
